bf138b0227517f3d18f60b19a6bbf57fc8f37a34
Pedro Ivan Juárez Cornejo <ivanjuarez@MacBook-Pro-de-Pedro.local>
2025-04-10 19:02:16 -0600

    Actualizando precios

--- src/app/modelos/cotizador.ts
+++ src/app/modelos/cotizador.ts
@@ -1,349 +1,348 @@
-export const baseDatos= [
+export const baseDatos = [
     {
-        destino: "Altamira",
-        hasta33T: 55000,
-        hasta39T: 58300,
-        hasta46T: 64900,
-        piloto1: 12000,
-        piloto2: 16000
+        "destino": "Altamira",
+        "hasta33T": 60000,
+        "hasta39T": 63600,
+        "hasta46T": 70800,
+        "piloto1": 14000,
+        "piloto2": 18500
     },
     {
-        destino: "Angostura Chiapas",
-        hasta33T: 75000,
-        hasta39T: 79500,
-        hasta46T: 88500,		
-        piloto1:  18000,
-        piloto2:  20000
+        "destino": "Angostura Chiapas",
+        "hasta33T": 81000,
+        "hasta39T": 85860,
+        "hasta46T": 95580,
+        "piloto1": 21000,
+        "piloto2": 23000
     },
     {
-        destino: "Aguascalientes",				
-        hasta33T: 90000,
-        hasta39T: 95400,
-        hasta46T: 106200,		
-        piloto1:  16000,
-        piloto2:  20000
+        "destino": "Aguascalientes",
+        "hasta33T": 97000,
+        "hasta39T": 102820,
+        "hasta46T": 114460,
+        "piloto1": 18000,
+        "piloto2": 23000
     },
     {
-        destino: "Cancun",									
-        hasta33T: 105000,
-        hasta39T: 111300,
-        hasta46T: 123900,		
-        piloto1:  20000,
-        piloto2:  24000
+        "destino": "Cancun",
+        "hasta33T": 113000,
+        "hasta39T": 119780,
+        "hasta46T": 133340,
+        "piloto1": 23000,
+        "piloto2": 27500
     },
     {
-        destino: "Campeche",														
-        hasta33T: 80000,
-        hasta39T: 84800,
-        hasta46T: 94400,		
-        piloto1:  14000,
-        piloto2:  18000
+        "destino": "Campeche",
+        "hasta33T": 86000,
+        "hasta39T": 91160,
+        "hasta46T": 101480,
+        "piloto1": 16000,
+        "piloto2": 21000
     },
     {
-        destino: "CDMX",																			
-        hasta33T: 52000,
-        hasta39T: 55120,
-        hasta46T: 61360,		
-        piloto1:  10000,
-        piloto2:  14000
+        "destino": "CDMX",
+        "hasta33T": 56000,
+        "hasta39T": 59360,
+        "hasta46T": 66080,
+        "piloto1": 11500,
+        "piloto2": 16000
     },
     {
-        destino: "Celaya",																								
-        hasta33T: 66000,
-        hasta39T: 69960,
-        hasta46T: 77880,		
-        piloto1:  12000,
-        piloto2:  16000
+        "destino": "Celaya",
+        "hasta33T": 71000,
+        "hasta39T": 75260,
+        "hasta46T": 83780,
+        "piloto1": 14000,
+        "piloto2": 18000
     },
     {
-        destino: "Ciudad del Carmen",																													
-        hasta33T: 65000,
-        hasta39T: 68900,
-        hasta46T: 76700,		
-        piloto1:  13500,
-        piloto2:  18000
+        "destino": "Ciudad del Carmen",
+        "hasta33T": 70000,
+        "hasta39T": 74200,
+        "hasta46T": 82600,
+        "piloto1": 15500,
+        "piloto2": 20000
     },
     {
-        destino: "Chihuhua, Chihuahua",																																	
-        hasta33T: 185000,
-        hasta39T: 196100,
-        hasta46T: 218300,		
-        piloto1:  34000,
-        piloto2:  42000	
+        "destino": "Chihuhua, Chihuahua",
+        "hasta33T": 199000,
+        "hasta39T": 210940,
+        "hasta46T": 234820,
+        "piloto1": 39000,
+        "piloto2": 48000
     },
     {
-        destino: "Coatzacoalcos",																																						
-        hasta33T: 35000,
-        hasta39T: 37100,
-        hasta46T: 41300,		
-        piloto1:  10000,
-        piloto2:  14000	
+        "destino": "Coatzacoalcos",
+        "hasta33T": 40000,
+        "hasta39T": 42400,
+        "hasta46T": 47200,
+        "piloto1": 11500,
+        "piloto2": 16000
     },
     {
-        destino: "Cuautla Morelos / Juitepec",																																											
-        hasta33T: 52000,
-        hasta39T: 55120,
-        hasta46T: 61360,		
-        piloto1:  12000,
-        piloto2:  16000	
+        "destino": "Cuautla Morelos / Juitepec",
+        "hasta33T": 57000,
+        "hasta39T": 60420,
+        "hasta46T": 67260,
+        "piloto1": 13500,
+        "piloto2": 18000
     },
     {
-        destino: "Culiacan Sinaloa",																																																
-        hasta33T: 160000,
-        hasta39T: 169600,
-        hasta46T: 188800,		
-        piloto1:  22000,
-        piloto2:  28000	
+        "destino": "Culiacan Sinaloa",
+        "hasta33T": 172000,
+        "hasta39T": 182320,
+        "hasta46T": 202960,
+        "piloto1": 25000,
+        "piloto2": 32000
     },
     {
-        destino: "Dos Bocas",																																																					
-        hasta33T: 55000,
-        hasta39T: 58300,
-        hasta46T: 64900,		
-        piloto1:  12000,
-        piloto2:  16000	
+        "destino": "Dos Bocas",
+        "hasta33T": 59000,
+        "hasta39T": 62540,
+        "hasta46T": 69620,
+        "piloto1": 14000,
+        "piloto2": 18000
     },
     {
-        destino: "Durango",																																																										
-        hasta33T: 115000,
-        hasta39T: 121900,
-        hasta46T: 135700,		
-        piloto1:  20000,
-        piloto2:  24000	
+        "destino": "Durango",
+        "hasta33T": 124000,
+        "hasta39T": 131440,
+        "hasta46T": 146320,
+        "piloto1": 23000,
+        "piloto2": 27000
     },
     {
-        destino: "Edo de Mex",																																																															
-        hasta33T: 52000,
-        hasta39T: 55120,
-        hasta46T: 61360,		
-        piloto1:  10000,
-        piloto2:  14000	
+        "destino": "Edo de Mex",
+        "hasta33T": 56000,
+        "hasta39T": 59360,
+        "hasta46T": 66080,
+        "piloto1": 11500,
+        "piloto2": 16000
     },
     {
-        destino: "Guadalajara",																																																																				
-        hasta33T: 85000,
-        hasta39T: 90100,
-        hasta46T: 100300,		
-        piloto1:  15000,
-        piloto2:  20000	
+        "destino": "Guadalajara",
+        "hasta33T": 92000,
+        "hasta39T": 97520,
+        "hasta46T": 108560,
+        "piloto1": 17000,
+        "piloto2": 23000
     },
     {
-        destino: "Guanajuato",																																																																									
-        hasta33T: 80000,
-        hasta39T: 84800,
-        hasta46T: 94400,		
-        piloto1:  14500,
-        piloto2:  18500	
+        "destino": "Guanajuato",
+        "hasta33T": 86000,
+        "hasta39T": 91160,
+        "hasta46T": 101480,
+        "piloto1": 16500,
+        "piloto2": 21000
     },
     {
-        destino: "Hermosillo Sonora",																																																																									
-        hasta33T: 185000,
-        hasta39T: 196100,
-        hasta46T: 218300,		
-        piloto1:  28000,
-        piloto2:  35000	
+        "destino": "Hermosillo Sonora",
+        "hasta33T": 199000,
+        "hasta39T": 210940,
+        "hasta46T": 234820,
+        "piloto1": 32000,
+        "piloto2": 40000
     },
     {
-        destino: "Irapuato",																																																																														
-        hasta33T: 75000,
-        hasta39T: 79500,
-        hasta46T: 88500,		
-        piloto1:  14000,
-        piloto2:  18000	
+        "destino": "Irapuato",
+        "hasta33T": 81000,
+        "hasta39T": 85860,
+        "hasta46T": 95580,
+        "piloto1": 16000,
+        "piloto2": 20000
     },
     {
-        destino: "Leon, Gto",																																																																																			
-        hasta33T: 80000,
-        hasta39T: 84800,
-        hasta46T: 94400,		
-        piloto1:  14500,
-        piloto2:  18500	
+        "destino": "Leon, Gto",
+        "hasta33T": 86000,
+        "hasta39T": 91160,
+        "hasta46T": 101480,
+        "piloto1": 16500,
+        "piloto2": 21000
     },
     {
-        destino: "Lagos de Moreno",																																																																																								
-        hasta33T: 85000,
-        hasta39T: 90100,
-        hasta46T: 100300,		
-        piloto1:  15000,
-        piloto2:  20000	
+        "destino": "Lagos de Moreno",
+        "hasta33T": 92000,
+        "hasta39T": 97520,
+        "hasta46T": 108560,
+        "piloto1": 17000,
+        "piloto2": 23000
     },
     {
-        destino: "Lazaro Cardenas",																																																																																													
-        hasta33T: 88000,
-        hasta39T: 93280,
-        hasta46T: 103840,		
-        piloto1:  16000,
-        piloto2:  22000	
+        "destino": "Lazaro Cardenas",
+        "hasta33T": 97000,
+        "hasta39T": 102820,
+        "hasta46T": 114460,
+        "piloto1": 18500,
+        "piloto2": 25000
     },
     {
-        destino: "Malpaso Chiapas",																																																																																																	
-        hasta33T: 60000,
-        hasta39T: 63600,
-        hasta46T: 70800,		
-        piloto1:  15000,
-        piloto2:  18000		
+        "destino": "Malpaso Chiapas",
+        "hasta33T": 64000,
+        "hasta39T": 67840,
+        "hasta46T": 75520,
+        "piloto1": 17000,
+        "piloto2": 20000
     },
     {
-        destino: "Manzanillo",																																																																																																						
-        hasta33T: 105000,
-        hasta39T: 111300,
-        hasta46T: 123900,		
-        piloto1:  20000,
-        piloto2:  24000		
-    },					
-    {
-        destino: "Merida",																																																																																																						
-        hasta33T: 85000,
-        hasta39T: 90100,
-        hasta46T: 100300,		
-        piloto1:  15000,
-        piloto2:  20000		
+        "destino": "Manzanillo",
+        "hasta33T": 113000,
+        "hasta39T": 119780,
+        "hasta46T": 133340,
+        "piloto1": 23000,
+        "piloto2": 27500
     },
     {
-        destino: "Mexicali Sonora",																																																																																																											
-        hasta33T: 285000,
-        hasta39T: 302100,
-        hasta46T: 336300,		
-        piloto1:  45000,
-        piloto2:  60000		
+        "destino": "Merida",
+        "hasta33T": 92000,
+        "hasta39T": 97520,
+        "hasta46T": 108560,
+        "piloto1": 17000,
+        "piloto2": 23000
     },
     {
-        destino: "Monterrey",																																																																																																																
-        hasta33T: 85000,
-        hasta39T: 90100,
-        hasta46T: 100300,		
-        piloto1:  16000,
-        piloto2:  20000		
-    },	
-    {  destino: "Morelia",																																																																																																																				
-       hasta33T:74000 ,
-       hasta39T:78440 ,
-       hasta46T:87320 ,		
-       piloto1: 14000 ,
-       piloto2: 18000		
-
-    },			
-    {
-        destino: "Peñitas, Chiapas",																																																																																																																
-        hasta33T: 52000,
-        hasta39T: 55120,
-        hasta46T: 61360,		
-        piloto1:  14000,
-        piloto2:  16000		
-    },  
-    {
-        destino: "Puebla, Puebla",																																																																																																																				
-        hasta33T: 38000,
-        hasta39T: 40280,
-        hasta46T: 44840,		
-        piloto1:  10000,
-        piloto2:  14000			
+        "destino": "Mexicali Sonora",
+        "hasta33T": 307000,
+        "hasta39T": 325420,
+        "hasta46T": 362260,
+        "piloto1": 51000,
+        "piloto2": 70000
     },
     {
-        destino: "Queretaro, Qro",																																																																																																																									
-        hasta33T: 56000,
-        hasta39T: 59360,
-        hasta46T: 66080,		
-        piloto1:  12000,
-        piloto2:  16000			
+        "destino": "Monterrey",
+        "hasta33T": 91000,
+        "hasta39T": 96460,
+        "hasta46T": 107380,
+        "piloto1": 18500,
+        "piloto2": 23000
     },
     {
-        destino: "Salina Cruz",																																																																																																																														
-        hasta33T: 48000,
-        hasta39T: 50880,
-        hasta46T: 56640,		
-        piloto1:  10000,
-        piloto2:  14000			
-    },					
-    {
-        destino: "Saltillo-Ramos Arizpe",																																																																																																																														
-        hasta33T: 98000,
-        hasta39T: 103880,
-        hasta46T: 115640,		
-        piloto1:  18000,
-        piloto2:  22000			
+        "destino": "Morelia",
+        "hasta33T": 79000,
+        "hasta39T": 83740,
+        "hasta46T": 93220,
+        "piloto1": 16000,
+        "piloto2": 21000
     },
     {
-        destino: "San Miguel Allende",																																																																																																																																			
-        hasta33T: 66000,
-        hasta39T: 69960,
-        hasta46T: 77880,		
-        piloto1:  12000,
-        piloto2:  16000			
+        "destino": "Peñitas, Chiapas",
+        "hasta33T": 56000,
+        "hasta39T": 59360,
+        "hasta46T": 66080,
+        "piloto1": 16000,
+        "piloto2": 18000
     },
     {
-        destino: "San Luis Potosi, SLP",																																																																																																																																								
-        hasta33T: 82000,
-        hasta39T: 86920,
-        hasta46T: 96760,		
-        piloto1:  15000,
-        piloto2:  20000			
-    },					
-    {
-        destino: "Silao",																																																																																																																																								
-        hasta33T: 78000,
-        hasta39T: 82680,
-        hasta46T: 92040,		
-        piloto1:  14500,
-        piloto2:  18500			
+        "destino": "Puebla, Puebla",
+        "hasta33T": 41000,
+        "hasta39T": 43460,
+        "hasta46T": 48380,
+        "piloto1": 11500,
+        "piloto2": 16000
     },
     {
-        destino: "Tlaxcala, Tlaxcala",																																																																																																																																													
-        hasta33T: 38000,
-        hasta39T: 40280,
-        hasta46T: 44840,		
-        piloto1:  10000,
-        piloto2:  14000			
-    },			
-    {
-        destino: "Tijuana",																																																																																																																																													
-        hasta33T: 305000,
-        hasta39T: 323300,
-        hasta46T: 359900,		
-        piloto1:  55000,
-        piloto2:  70000			
+        "destino": "Queretaro, Qro",
+        "hasta33T": 60000,
+        "hasta39T": 63600,
+        "hasta46T": 70800,
+        "piloto1": 14000,
+        "piloto2": 18000
     },
     {
-        destino: "Toluca",																																																																																																																																																		
-        hasta33T: 55000,
-        hasta39T: 58300,
-        hasta46T: 64900,		
-        piloto1:  12000,
-        piloto2:  16000			
+        "destino": "Salina Cruz",
+        "hasta33T": 56000,
+        "hasta39T": 59360,
+        "hasta46T": 66080,
+        "piloto1": 11500,
+        "piloto2": 16000
     },
     {
-        destino: "Torreon",																																																																																																																																																							
-        hasta33T: 110000,
-        hasta39T: 116600,
-        hasta46T: 129800,		
-        piloto1:  16000,
-        piloto2:  20000			
-    },					
-    {
-        destino: "Tuxpan",																																																																																																																																																							
-        hasta33T: 48000,
-        hasta39T: 50880,
-        hasta46T: 56640,		
-        piloto1:  10000,
-        piloto2:  14000			
-    },					
-    {
-        destino: "Villahermosa",																																																																																																																																																							
-        hasta33T: 58000,
-        hasta39T: 61480,
-        hasta46T: 68440,		
-        piloto1:  12000,
-        piloto2:  16000			
-    },				
-    {
-        destino: "Zacatecas",																																																																																																																																																							
-        hasta33T: 102000,
-        hasta39T: 108120,
-        hasta46T: 120360,		
-        piloto1:  18000,
-        piloto2:  22000			
+        "destino": "Saltillo-Ramos Arizpe",
+        "hasta33T": 105000,
+        "hasta39T": 111300,
+        "hasta46T": 123900,
+        "piloto1": 21000,
+        "piloto2": 25000
     },
-    
+    {
+        "destino": "San Miguel Allende",
+        "hasta33T": 71000,
+        "hasta39T": 75260,
+        "hasta46T": 83780,
+        "piloto1": 14000,
+        "piloto2": 18000
+    },
+    {
+        "destino": "San Luis Potosi, SLP",
+        "hasta33T": 88000,
+        "hasta39T": 93280,
+        "hasta46T": 103840,
+        "piloto1": 17000,
+        "piloto2": 23000
+    },
+    {
+        "destino": "Silao",
+        "hasta33T": 84000,
+        "hasta39T": 89040,
+        "hasta46T": 99120,
+        "piloto1": 16500,
+        "piloto2": 21000
+    },
+    {
+        "destino": "Tlaxcala, Tlaxcala",
+        "hasta33T": 41000,
+        "hasta39T": 43460,
+        "hasta46T": 48380,
+        "piloto1": 11500,
+        "piloto2": 16000
+    },
+    {
+        "destino": "Tijuana",
+        "hasta33T": 329000,
+        "hasta39T": 348740,
+        "hasta46T": 388220,
+        "piloto1": 63000,
+        "piloto2": 80500
+    },
+    {
+        "destino": "Toluca",
+        "hasta33T": 59000,
+        "hasta39T": 62540,
+        "hasta46T": 69620,
+        "piloto1": 14000,
+        "piloto2": 18000
+    },
+    {
+        "destino": "Torreon",
+        "hasta33T": 118000,
+        "hasta39T": 125080,
+        "hasta46T": 139240,
+        "piloto1": 18000,
+        "piloto2": 23000
+    },
+    {
+        "destino": "Tuxpan",
+        "hasta33T": 51000,
+        "hasta39T": 54060,
+        "hasta46T": 60180,
+        "piloto1": 11500,
+        "piloto2": 16000
+    },
+    {
+        "destino": "Villahermosa",
+        "hasta33T": 62000,
+        "hasta39T": 65720,
+        "hasta46T": 73160,
+        "piloto1": 14000,
+        "piloto2": 18000
+    },
+    {
+        "destino": "Zacatecas",
+        "hasta33T": 110000,
+        "hasta39T": 116600,
+        "hasta46T": 129800,
+        "piloto1": 21000,
+        "piloto2": 25000
+    }
 ]
 export const users =  {
         uid: 'fE2oJKb0jFfvfhv8KG6nJ2btUww1',
