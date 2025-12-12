d06b56dae8ada5555a0c5e82cde5dc879f42bd67
Pedro Ivan Juárez Cornejo <ivanjuarez@MacBook-Pro-de-Pedro.local>
2025-12-12 12:46:15 -0600

    Ajustando guardian para validar la sesion

--- src/app/servicios/guard.service.ts
+++ src/app/servicios/guard.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { UserService } from './users.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard {
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private router: Router) {}
 
   canActivate(): Promise<boolean> {
     return new Promise((resolve) => {
       const auth = getAuth();
+   
       onAuthStateChanged(auth, (user) => {
-        if (user) {
+        console.log(user);
+        if (this.validateSession(auth, user)) {
           resolve(true);
         } else {
           this.router.navigate(['/']);
@@ -22,5 +23,15 @@ export class AuthGuard implements CanActivate {
         }
       });
     });
+  }
+  validateSession(auth : any, user: any) : Boolean {
+    let userValid = false
+    try {
+      userValid =  auth.currentUser.uid == user.uid
+      return userValid;
+    } catch (error) {
+      console.log(error);
+      return userValid;
+    }
   }
 }
