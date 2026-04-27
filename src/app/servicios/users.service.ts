809d75e34659ce8cbd86a46d3fd51a3996f55d4d
Pedro Ivan Juárez Cornejo <ivanjuarez@MacBook-Pro-de-Pedro.local>
2026-04-27 12:42:37 -0600

    Limpiando logs

--- src/app/servicios/users.service.ts
+++ src/app/servicios/users.service.ts
@@ -27,7 +27,7 @@ export class UserService {
        createUserWithEmailAndPassword(this.auth, email, password)
         .then((userCredential) => {
             const user = userCredential.user;
-            console.log(user);
+
         })
         .catch((error) => {
             const errorCode = error.code;
